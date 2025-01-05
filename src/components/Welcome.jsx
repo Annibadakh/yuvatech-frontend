2aff108e5f8160c61f80bc1c03116947a0bdf51b
Aniket Badakh <aniketbadakh1212@gmail.com>
2025-01-05 19:30:01 +0530

    student dashboard home img updated

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 import { useSelector } from "react-redux";
-import studentdash from '../assets/studentdash.png';
+import studentdash from '../assets/education.jpg';
 const Welcome = () => {
   const { user } = useSelector((state) => state.auth);
   const apiUrl = process.env.REACT_APP_API_BASE_URL
   const titleStyle = {
-    color:"white"
+    color:"black"
   };
   const [date, setDate] = useState(new Date());
 
@@ -18,10 +18,10 @@ const Welcome = () => {
     return () => clearInterval(timer); // Cleanup on unmount
   }, []);
   return (
-    <div style={{position:"relative"}}>
-      <img style={{width:"100%"}} src={studentdash} alt="" />
-      <div style={{position:"absolute", top:"15px", left:"10px"}}>
-      <h2 style={{marginBottom:"10px", color:"white"}}>Date: {date.toLocaleDateString()}</h2>
+    <div style={{position:"relative", display:"flex", justifyContent:"end", alignContent:"center", padding:"20px"}}>
+      <img style={{height:"350px", width:"auto"}} src={studentdash} alt="" />
+      <div style={{position:"absolute", top:"50px", left:"10px"}}>
+      <h2 style={{marginBottom:"10px", color:"black"}}>Date: {date.toLocaleDateString()}</h2>
       <h1 className="subtitle" style={titleStyle}>
         Welcome Back {user && user.name}
       </h1>
